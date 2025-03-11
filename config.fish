25620d133dfa7802f6bee125e330fe418fc9ae0d
hewigovens <360470+hewigovens@users.noreply.github.com>
2025-03-11 09:07:10 +0900

    add reverse_hex

--- config.fish
+++ config.fish
@@ -67,6 +67,34 @@ function local_ip
     ifconfig | grep broadcast | awk '{print $2}'
 end
 
+function reverse_hex
+    set str $argv[1]
+
+    # Remove '0x' prefix if present
+    if string match -q "0x*" $str
+        set str (string sub -s 3 $str)
+    end
+
+    # Add leading zero if length is odd
+    set len (string length $str)
+    set remainder (math "$len % 2")
+    if test $remainder -ne 0
+        set str "0$str"
+    end
+
+    # Split the hex string into pairs of characters
+    set pairs
+    for i in (seq 1 2 (string length $str))
+        set -a pairs (string sub -s $i -l 2 $str)
+    end
+
+    # Reverse the pairs and join them
+    set reversed_pairs $pairs[-1..1]
+    set reversed (string join "" $reversed_pairs)
+
+    echo $reversed
+end
+
 if test (uname) = Darwin
     # CocoaPods
     alias pdi="pod install --verbose"
