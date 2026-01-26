fc91b96ab397b3512f796817518ed9bba15ca002
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-01-26 20:02:37 +0900

    update 01-ssh.fish to support macOS Keychain for SSH keys

--- fish/conf.d/01-ssh.fish
+++ fish/conf.d/01-ssh.fish
@@ -1,6 +1,14 @@
 if status is-interactive && set -q SSH_TTY
-    if test (uname) = Darwin
-        # Use macOS Keychain for SSH keys - agent is managed by launchd
-        set -gx SSH_ASKPASS /usr/bin/ssh-askpass
+    set -l agent_file ~/.ssh/agent.fish
+    if ssh-add -l >/dev/null 2>&1
+    else
+        if test -f $agent_file
+            source $agent_file >/dev/null 2>&1
+        end
+
+        if not ssh-add -l >/dev/null 2>&1
+            eval (ssh-agent -c | tee $agent_file)
+            ssh-add 2>/dev/null
+        end
     end
 end
